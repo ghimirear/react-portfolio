e2b87d5a639df4608cfcbb3834f10dfdc02fc836
Arjun Ghimire <ghimirearjun15@gmail.com>
2021-09-02 17:42:36 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-//import logo from './logo.svg';
-import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import NavBar from "./components/elements/navbar";
-import Footer from "./components/elements/footer";
-import Home from "./components/pages/Home";
-import Contact from "./components/pages/Contact.js";
-import Portfolio from "./components/pages/Portfolio";
-import ScrollToTop from "./components/ScrollTop";
-import Preloader from "./components/Pre";
-function App() {
-  const [load, upadateLoad] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
-  }, []);
-  return (
-    <Router>
-      <Preloader load={load} />
-      <div className="App mb-0" id={load ? "no-scroll" : "scroll"}>
-        <NavBar />
-        <ScrollToTop />
-        <Switch>
-          < Route  path ={["/", "*"]}> < Home />  </Route>
-        <Route exact path="/portfolio"  ><Portfolio />  </Route>
-        <Route exact path="/contact"> <Contact /> </Route>
-        </Switch >
-        <Footer />
-      </div>
+// //import logo from './logo.svg';
+// import React, { useEffect, useState } from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import NavBar from "./components/elements/navbar";
+// import Footer from "./components/elements/footer";
+// import Home from "./components/pages/Home";
+// import Contact from "./components/pages/Contact.js";
+// import Portfolio from "./components/pages/Portfolio";
+// import ScrollToTop from "./components/ScrollTop";
+// import Preloader from "./components/Pre";
+// function App() {
+//   const [load, upadateLoad] = useState(true);
+//   useEffect(() => {
+//     setTimeout(() => {
+//       upadateLoad(false);
+//     }, 1200);
+//   }, []);
+//   return (
+//     <Router>
+//       <Preloader load={load} />
+//       <div className="App mb-0" id={load ? "no-scroll" : "scroll"}>
+//         <NavBar />
+//         <ScrollToTop />
+//         <Switch>
+//           < Route  path ={["/", "*"]}> < Home />  </Route>
+//         <Route exact path="/portfolio"  ><Portfolio />  </Route>
+//         <Route exact path="/contact"> <Contact /> </Route>
+//         </Switch >
+//         <Footer />
+//       </div>
       
-    </Router>
-  );
-}
+//     </Router>
+//   );
+// }
 
+// export default A
+import React from"react";
+import PortfolioContainer from "./components/portfoliocontainer";
+function App (){
+<div className="App">
+  <PortfolioContainer />
+</div>
+}
 export default App;
--- src/components/portfoliocontainer.js
+++ src/components/portfoliocontainer.js
@@ -1,34 +1,34 @@
-// import React, { useEffect, useState } from "react";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import NavBar from "./elements/navbar";
-// import Footer from "./elements/footer";
-// import Home from "./pages/Home";
-// import Contact from "./pages/Contact";
-// import Portfolio from "./pages/Portfolio";
-// import ScrollToTop from "./ScrollTop";
-// import Preloader from "./Pre";
-// function PortfolioContainer(){
-//   const [load, upadateLoad] = useState(true);
-//   useEffect(() => {
-//     setTimeout(() => {
-//       upadateLoad(false);
-//     }, 1200);
-//   }, []);
-//   return (
-//     <Router>
-//       <Preloader load={load} />
-//       <div className="App pb-0" id={load ? "no-scroll" : "scroll"}>
-//         <NavBar />
-//         <ScrollToTop />
-//         <Switch>
-//        <Home />
-//         <Route exact path="/portfolio" > <Portfolio /> </Route> 
-//         <Route exact path="/contact"> < Contact/> </Route>
-//         </Switch>
-//         <Footer />
-//       </div>
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from "./elements/navbar";
+import Footer from "./elements/footer";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import ScrollToTop from "./ScrollTop";
+import Preloader from "./Pre";
+function PortfolioContainer(){
+  const [load, upadateLoad] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+  }, []);
+  return (
+    <Router>
+      <Preloader load={load} />
+      <div className="App pb-0" id={load ? "no-scroll" : "scroll"}>
+        <NavBar />
+        <ScrollToTop />
+        <Switch>
+       <Home />
+        <Route exact path="/portfolio" > <Portfolio /> </Route> 
+        <Route exact path="/contact"> < Contact/> </Route>
+        </Switch>
+        <Footer />
+      </div>
       
-//     </Router>
-//   );
-// }
-// export default PortfolioContainer;
+    </Router>
+  );
+}
+export default PortfolioContainer;
